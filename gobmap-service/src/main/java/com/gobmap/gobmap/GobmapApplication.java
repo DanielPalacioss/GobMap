129d909636f84165972c61e4006547dde64c36d2
Daniel <arindaniel17@gmail.com>
2025-06-24 23:20:14 -0500

    Correción de src

---
+++ gobmap-service/src/main/java/com/gobmap/gobmap/GobmapApplication.java
@@ -0,0 +1,13 @@
+package com.gobmap.gobmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobmapApplication.class, args);
+	}
+
+}
---
+++ gobmap-service/src/test/java/com/gobmap/gobmap/GobmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobmap.gobmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GobmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
