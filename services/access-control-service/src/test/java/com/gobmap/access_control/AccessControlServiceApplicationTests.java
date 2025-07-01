5e9abed3465f9043c109a0dc6de5cd6346d90f97
Daniel <arindaniel17@gmail.com>
2025-06-30 20:20:42 -0500

    se suben los cambios mencionados en commit anteriores

---
+++ services/access-control-service/src/test/java/com/gobmap/access_control/AccessControlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gobmap.access_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
